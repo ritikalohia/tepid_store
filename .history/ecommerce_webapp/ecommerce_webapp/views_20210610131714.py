68d6849e332041a8a818cc62f5129a75c875172f
ritika <rtklohia@gmail.com>
2021-06-10 17:15:09 +0530

    2nd commit

---
+++ .history/ecommerce_webapp/ecommerce_webapp/views_20210610131714.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def productView(request):
+    return HttpResponse("We are at product view")
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')      
---
+++ .history/ecommerce_webapp/ecommerce_webapp/views_20210610132900.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+
+def account(request):
+    return render(request, "login_signin")
+
+def productView(request):
+    return render(request, "product_view.html")
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')      
