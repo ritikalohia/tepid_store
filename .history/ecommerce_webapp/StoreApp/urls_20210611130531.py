5cb988f7a931829cba4b55a3895227f71efe6a4f
ritika <rtklohia@gmail.com>
2021-06-17 10:44:14 +0530

    4th commit

---
+++ .history/ecommerce_webapp/StoreApp/urls_20210611130531.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path("account/", views.account, name="Account"),
+    path("aboutus/", views.about, name="AboutUs"),
+    #path("tracker/", views.tracker, name="TrackingStatus"),
+    path("producview/", views.productView, name="ProductView"),
+    path("products/", views.products, name="Product"),
+    path("checkout/", views.checkout, name="Checkout"),
+]
---
+++ .history/ecommerce_webapp/StoreApp/views_20210611130748.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse 
+from django.shortcuts import render
+from . models import Product
+
+
+def index(request):
+    return render(request, 'index.html')
+
+# def tracker(request):
+#     return render(request, 'shop/tracker.html')
+
+def account(request):
+    if request.method=="POST":
+        print(request)
+        name = request.POST.get('name', ' ')
+        email = request.POST.get('email', ' ')
+        password = request.POST.get('password', ' ')
+        contact = account(name=name, email=email, password=password)
+        contact.save()
+    return render(request, "login_signin.html")
+
+def productView(request):
+    return render(request, "product_view.html")
+
+def checkout(request):
+    return render(request, 'checkout.html')    
+
+def products(request):
+    products = Product.objects.all()
+    context={'prods': products}
+    #print(products)
+    return render(request, "products.html", context)
+
+def about(request):
+    return render(request, "aboutus.html")    
